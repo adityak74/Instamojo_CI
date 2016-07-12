d36bf8af9f9f3ee36aedd86101debf0f4214871d
Aditya K G <akarnam37@gmail.com>
2016-07-12 12:20:47 +0530

    Initial Commit

---
+++ application/config/instamojo.php
@@ -0,0 +1,14 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+/*
+ * Instamojo File Config
+ *
+ * File     : instamojo.php
+ * Created  : Mon 11, 2016 | 11:18:00 PM
+ * 
+ * Author   : Aditya Karnam <akarnam37@gmail.com>
+ */
+//Instamojo API Key and Auth Token
+$config['api_key']   = 'YOUR API KEY';
+$config['auth_token']  = 'YOUR AUTH TOKEN';
+
+?>
